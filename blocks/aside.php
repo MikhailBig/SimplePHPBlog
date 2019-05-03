842db219a8c56e9bb0276ae51688aa137debfa31
Mikhail <mikhail.bigun95@gmail.com>
2019-05-03 23:18:00 +0300

    new files added

---
+++ blocks/aside.php
@@ -0,0 +1,20 @@
+<aside class="col-md-4 blog-sidebar">
+  <div class="p-4 mb-3 bg-warning rounded">
+    <h4 class="font-italic">About</h4>
+    <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+  </div>
+
+  <div class="p-4">
+    <h4 class="font-italic">Ads</h4>
+    <a href=""><img class="img-thumbnail" src="https://imagescdn.dealercarsearch.com/Media/14883/12560787/636897243426080342.jpg" alt="ads image"></a>
+  </div>
+
+  <div class="p-4">
+    <h4 class="font-italic">Elsewhere</h4>
+    <ol class="list-unstyled">
+      <li><a href="https://github.com">GitHub</a></li>
+      <li><a href="https://twitter.com">Twitter</a></li>
+      <li><a href="https://www.facebook.com">Facebook</a></li>
+    </ol>
+  </div>
+</aside><!-- /.blog-sidebar -->
---
+++ blocks/footer.php
@@ -0,0 +1,8 @@
+<footer class="blog-footer">
+  <p>
+    <a href="#">Back to top</a>
+  </p>
+  <p>
+    <span class="text-muted">All rights reserved&#169;</span>
+  </p>
+</footer>
